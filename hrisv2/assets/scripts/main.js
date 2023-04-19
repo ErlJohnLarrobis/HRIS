cce9ce6882eead55583fc76af4b3b4f4f32f6119
Erl John Larrobis <60595663+ErlJohnLarrobis@users.noreply.github.com>
2023-04-19 08:45:12 +0800

    Add files via upload

---
+++ hrisv2/assets/scripts/main.js
@@ -0,0 +1,20 @@
+function recruitment(){
+  const fcollapse=document.getElementById('first')
+  fcollapse.classList.toggle('toggle')
+}
+function leaves(){
+  const scollapse=document.getElementById('second')
+  scollapse.classList.toggle('toggle')
+}
+function reports(){
+  const tcollapse=document.getElementById('third')
+  tcollapse.classList.toggle('toggle')
+}
+function collapse(){
+  const collapse=document.getElementById('collapse')
+  collapse.classList.toggle('to-collapse')
+}
+function uncollapse(){
+  const uncollapse=document.getElementById('collapse')
+  uncollapse.classList.toggle('to-collapse')
+}
